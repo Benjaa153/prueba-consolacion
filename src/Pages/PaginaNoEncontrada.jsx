c51f4e74598a367101878f8cc64264a069c7dbe8
Benjamin <goosta19802@gmail.com>
2022-11-28 14:35:46 -0300

    a

--- src/Pages/PaginaNoEncontrada.jsx
+++ src/Pages/PaginaNoEncontrada.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import '../Styles/paginaNoEncontrada.css'
+import Header from './Header'
 const PaginaNoEncontrada = () => {
     return (
-        <section className='page_404'>
-            <div className='container-page'>
-                <div className='row-page'>
-                    <div className='col-sm-12'>
-                        <div className='col-sm-10 col-sm-offset-1 text-center'>
-                            <div className='four_zero_four_bg'>
-                                <h1 className='h1-text'>404</h1>
-                            </div>
-                            <div className='contant_box_404'>
-                                <h3 className='h2-page'>Parece que estás perdido</h3>
-                                <p>¡La página que buscas no está disponible!</p>
-                                <a href='/' className='link_404'>Volver al Inicio</a>
+        <>
+            <Header/>
+            <section className='page_404'>
+                <div className='container-page'>
+                    <div className='row-page'>
+                        <div className='col-sm-12'>
+                            <div className='col-sm-10 col-sm-offset-1 text-center'>
+                                <div className='four_zero_four_bg'>
+                                    <h1 className='h1-text'>404</h1>
+                                </div>
+                                <div className='contant_box_404'>
+                                    <h3 className='h2-page'>Parece que estás perdido</h3>
+                                    <p>¡La página que buscas no está disponible!</p>
+                                    <a href='/' className='link_404'>Volver al Inicio</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
